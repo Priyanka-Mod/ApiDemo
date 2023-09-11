9fadaf694937feeaa67073c1720a53285aa199b9
kevit-priyanka-mod <141119585+kevit-priyanka-mod@users.noreply.github.com>
2023-09-11 17:50:15 +0530

    javascript_task-1

---
+++ js-task/index.js
@@ -0,0 +1,14 @@
+fetch("https://reqres.in/api/users?page=1&&per_page=15")
+.then((data)=> data.json())  //this will convert data into object 
+.then((objectData)=>{
+    console.log(objectData)
+    let tableData="";
+    objectData.data.map((user) => {
+        tableData += `<tr>
+          <td><img src='${user.avatar}'/></td>
+          <td>${user.first_name} </td>
+          <td>${user.last_name}</td>
+        </tr>`;
+      });
+      document.getElementById("table__body").innerHTML = tableData; 
+});
